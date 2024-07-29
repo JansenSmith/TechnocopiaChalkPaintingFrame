1fbd6e1fa8a6869279fd31d2d7491f48f9942f97
Jansen Smith <jansen@bstarengineering.com>
2024-07-29 17:09:13 -0400

    imported stl into script

--- TechnocopiaChalkPaintingFrame.groovy
+++ TechnocopiaChalkPaintingFrame.groovy
@@ -1,1 +1,8 @@
-// code here
+// import logo STL
+File logoSTL = ScriptingEngine.fileFromGit(
+	"https://github.com/JansenSmith/TechnocopiaChalkPaintingFrame.git",
+	"Technocopia_chalk_painting_simplified.stl");
+// Load the .CSG from the disk and cache it in memory
+CSG logo  = Vitamins.get(logoSTL);
+logo = logo.toZMin()
+//logo = logo.hull()
