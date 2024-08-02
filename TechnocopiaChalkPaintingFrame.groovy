7f3f89a4969b2bccf6d9fef552767d2f8fad2575
Jansen Smith <jansen@bstarengineering.com>
2024-08-02 12:00:12 -0400

    changing script to load manifold STL

--- TechnocopiaChalkPaintingFrame.groovy
+++ TechnocopiaChalkPaintingFrame.groovy
@@ -1,17 +1,20 @@
+import com.neuronrobotics.bowlerstudio.util.GeometrySimplification
+
 // import logo STL
 File logoSTL = ScriptingEngine.fileFromGit(
 	"https://github.com/JansenSmith/TechnocopiaChalkPaintingFrame.git",
-	"Technocopia_chalk_painting_simplified_original.stl");
+	"Technocopia_chalk_painting_simplified_manifold.stl");
+
 // Load the .CSG from the disk and cache it in memory
 CSG logo  = Vitamins.get(logoSTL);
 logo = logo.toZMin()
 //logo = logo.hull()
 
 
-List<Polygon> polys = Slice.slice(logo,new Transform(),0)
-Polygon shp = polys.get(0)
+//List<Polygon> polys = Slice.slice(logo,new Transform(),0)
+//Polygon shp = polys.get(0)
 
-CSG cutout = Extrude.polygons(shp, 1.2)
-cutout = cutout.setColor(javafx.scene.paint.Color.RED)
+//CSG cutout = Extrude.polygons(shp, 1.2)
+//cutout = cutout.setColor(javafx.scene.paint.Color.RED)
 
-return [cutout, logo]
+return logo
