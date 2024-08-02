f852db935333da2cee7751f13fa5b2266cdbc9ae
Jansen Smith <jansen@bstarengineering.com>
2024-08-02 11:19:33 -0400

    Ignore the project files

--- TechnocopiaChalkPaintingFrame.groovy
+++ TechnocopiaChalkPaintingFrame.groovy
@@ -1,8 +1,17 @@
 // import logo STL
 File logoSTL = ScriptingEngine.fileFromGit(
 	"https://github.com/JansenSmith/TechnocopiaChalkPaintingFrame.git",
-	"Technocopia_chalk_painting_simplified.stl");
+	"Technocopia_chalk_painting_simplified_original.stl");
 // Load the .CSG from the disk and cache it in memory
 CSG logo  = Vitamins.get(logoSTL);
 logo = logo.toZMin()
 //logo = logo.hull()
+
+
+List<Polygon> polys = Slice.slice(logo,new Transform(),0)
+Polygon shp = polys.get(0)
+
+CSG cutout = Extrude.polygons(shp, 1.2)
+cutout = cutout.setColor(javafx.scene.paint.Color.RED)
+
+return [cutout, logo]
